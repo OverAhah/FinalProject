4d829666a9858e15ce5d634f8fa5c7e2d5d0b13f
OverAhah <kozak.art@gmail.com>
2021-09-26 23:35:48 +0300

    fix some bugs in product page

--- pages/product_page.py
+++ pages/product_page.py
@@ -11,7 +11,7 @@ class ProductPage(BasePage):
         assert self.browser.find_element(*ProductPageLocators.MSG_FORM), "Message is not presented!"
 
     def should_be_product_name(self):
-        assert  self.browser.find_element(*ProductPageLocators.ASSERT_BOOK_NAME) in self.browser.find_element(*ProductPageLocators.BOOK_NAME), "Book name is different!"
+        assert  self.browser.find_element(*ProductPageLocators.ASSERT_BOOK_NAME) == self.browser.find_element(*ProductPageLocators.BOOK_NAME), "Book name is different!"
 
     def should_be_prise(self):
-        assert  self.browser.find_element(*ProductPageLocators.ASSERT_PRICE) in self.browser.find_element(*ProductPageLocators.PRiCE), "Price is different!"
+        assert  self.browser.find_element(*ProductPageLocators.ASSERT_PRICE) == self.browser.find_element(*ProductPageLocators.PRiCE), "Price is different!"
--- test_product_page.py
+++ test_product_page.py
@@ -3,13 +3,9 @@ from .pages.product_page import ProductPage
 
 def test_allert(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    #link = "https://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
     page = MainPage(browser, link)
     page.open()
     page.solve_quiz_and_get_code()
-
-def test_guest_can_add_product_to_basket(browser):
-    link = "https://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
     page = ProductPage(browser, link)
     page.open()
-    page.should_be_basket()
+    page.should_be_basket
