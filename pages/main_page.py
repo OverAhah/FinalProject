70734a5090435937fc31f5338d5b6d75ffc159e1
OverAhah <kozak.art@gmail.com>
2021-09-27 12:32:38 +0300

    add final changes

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,32 +1,21 @@
+import time
 from .base_page import BasePage
 from .locators import MainPageLocators
-from .locators import ProductPageLocators
-import math
-from selenium.common.exceptions import NoAlertPresentException
 
-class MainPage(BasePage): 
-    def go_to_login_page(self):        
-        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        link.click()
-        # return LoginPage(browser=self.browser, url=self.browser.current_url) 
 
-    def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
-    
-    def solve_quiz_and_get_code(self):
-        button = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
-        button.click()
-        alert = self.browser.switch_to.alert
-        x = alert.text.split(" ")[2]
-        answer = str(math.log(abs((12 * math.sin(float(x))))))
-        alert.send_keys(answer)
-        alert.accept()
-        try:
-            alert = self.browser.switch_to.alert
-            alert_text = alert.text
-            print(f"Your code: {alert_text}")
-            alert.accept()
-        except NoAlertPresentException:
-            print("No second alert presented")
-    
-    
+class MainPage(BasePage):
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
+
+    def should_be_browse_store(self):
+        assert self.is_element_present(*MainPageLocators.BROWSE_STORE), "Browse store link is not presented"
+
+    def all_products(self):
+        assert self.is_element_present(*MainPageLocators.ALL_BOOKS), "All books list is not presented"
+        text = self.browser.find_element(*MainPageLocators.ALL_BOOKS).get_attribute("innerHTML")
+        all_prod = self.browser.find_element(*MainPageLocators.ALL_BOOKS)
+        all_prod.click()
+        page_title = self.browser.find_element(*MainPageLocators.HEADER_ALLBOOKS)
+        title = page_title.get_attribute("innerHTML")
+        time.sleep(5)
+        assert text in title, "ALL PRODUCTS title is not presented"
